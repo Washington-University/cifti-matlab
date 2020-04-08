20e62d586856f83a820f69e9d911241d97181fe5
Tim Coalson <tsc5yc@mst.edu>
2020-04-08 15:10:01 -0500

    add missing field specifier in ciftisavereset

--- ciftisavereset.m
+++ ciftisavereset.m
@@ -14,7 +14,7 @@ function ciftisavereset(cifti, filename, varargin)
                 cifti.diminfo{2} = cifti_diminfo_make_series(size(cifti.cdata, 2));
             case '.sdseries.nii' %this case was not handled in the public version
                 cifti.diminfo{1} = cifti_diminfo_make_scalars(size(cifti.cdata, 1));
-                if size(cifti.cdata, 2) ~= cifti.diminfo{2}.length || ~strcmp(cifti.diminfo{2}, 'series')
+                if size(cifti.cdata, 2) ~= cifti.diminfo{2}.length || ~strcmp(cifti.diminfo{2}.type, 'series')
                     warning(['resetting series dimension on sdscalar file "' filename '"']); %this was not done in the original ciftisavereset
                     cifti.diminfo{2} = cifti_diminfo_make_series(size(cifti.cdata, 2)); %so don't do it except when needed, and give a warning
                 end
