81709489789896e856a5e7328cbd06ac7d8f81a3
Tim Coalson <tsc5yc@mst.edu>
2020-04-15 19:56:15 -0500

    also change sdseries helper to Name,Value convention

--- cifti_create_sdseries.m
+++ cifti_create_sdseries.m
@@ -1,24 +1,20 @@
-function cifti = cifti_create_sdseries(data, start, step, unit, namelist, metadatalist)
-    %function cifti = cifti_create_sdseries(data, start, step, unit, namelist, metadatalist)
+function cifti = cifti_create_sdseries(data, varargin)
+    %function cifti = cifti_create_sdseries(data, ...)
     %   Construct an sdseries cifti object around the 2D data matrix.
     %
-    %   Only the data argument is required.
-    if nargin < 2
-        start = 0;
+    %   To control the contents of the scalar and series diminfo, use 'start',
+    %   'step', 'unit', 'namelist', and 'metadatalist' options.
+    options = myargparse(varargin, {'start', 'step', 'unit', 'namelist', 'metadatalist'});
+    if isempty(options.start)
+        options.start = 0;
     end
-    if nargin < 3
-        step = 1;
+    if isempty(options.step)
+        options.step = 1;
     end
-    if nargin < 4
-        unit = 'SECOND'; %let make_series sanity check whatever the user gave
+    if isempty(options.unit)
+        options.unit = 'SECOND'; %let make_series sanity check whatever the user gave
     end
     cifti = struct('cdata', data, 'metadata', {{}}, 'diminfo', {cell(1, 2)});
-    if nargin < 5
-        cifti.diminfo{1} = cifti_diminfo_make_scalars(size(data, 1));
-    elseif nargin < 6
-        cifti.diminfo{1} = cifti_diminfo_make_scalars(size(data, 1), namelist);
-    else
-        cifti.diminfo{1} = cifti_diminfo_make_scalars(size(data, 1), namelist, metadatalist);
-    end
-    cifti.diminfo{2} = cifti_diminfo_make_series(size(data, 2), start, step, unit);
+    cifti.diminfo{1} = cifti_diminfo_make_scalars(size(data, 1), options.namelist, options.metadatalist);
+    cifti.diminfo{2} = cifti_diminfo_make_series(size(data, 2), options.start, options.step, options.unit);
 end
